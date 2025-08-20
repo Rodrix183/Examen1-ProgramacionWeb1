f21567c161528a1128048e75a037f95b6a3c7933
Rodrigo Velásquez <38990525+Rodrix183@users.noreply.github.com>
2025-08-19 20:41:53 -0600

    Colocar precios y restricciones

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,28 @@
 import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function App() {
+   // Guardamos los 5 precios como strings para facilitar validación
+  const [precios, setPrecios] = useState(["", "", "", "", ""]);
+
+  // Resultados
+  const [subtotal, setSubtotal] = useState(0);
+  const [porcentaje, setPorcentaje] = useState(0);
+  const [descuento, setDescuento] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  const onChangePrecio = (idx, value) => {
+    // Permitimos vacío o número >= 0
+    setPrecios((prev) => {
+      const copy = [...prev];
+      copy[idx] = value;
+      return copy;
+    });
+  };
+
+
+
+
+
+
+}
